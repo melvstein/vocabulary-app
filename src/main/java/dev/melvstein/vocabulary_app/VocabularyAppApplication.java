375a48fcf15fecd43618f5f12a2559b4e143031d
melvin.php.ph <melvin.php.ph@tripleonetech.net>
2025-12-07 20:23:51 +0800

    vocabulary app

---
+++ src/main/java/dev/melvstein/vocabulary_app/VocabularyAppApplication.java
@@ -0,0 +1,13 @@
+package dev.melvstein.vocabulary_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocabularyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocabularyAppApplication.class, args);
+	}
+
+}
